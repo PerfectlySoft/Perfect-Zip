016a09a855a17518b42a1444103237de367459c3
Jonathan Guthrie <jono@perfect.org>
2016-08-03 14:53:39 -0400

    move enums and structs into utilities

--- Sources/utilities.swift
+++ Sources/utilities.swift
@@ -1,18 +1,64 @@
 //
 //  utilities.swift
-//  ZipLib
+//  PerfectZip
 //
 //  Created by Jonathan Guthrie on 2016-08-02.
 //
 //
+// ==========================================================================
+//	loosely based on https://github.com/marmelroy/Zip
+//	converted to Swift 3 and utilizing Perfect 2's directory and file objects
+// ==========================================================================
 
 import PerfectLib
 import Foundation
 
+
+/// Zip status type
+public enum ZipStatus: Error {
+	/// File not found
+	case FileNotFound
+	/// Unzip fail
+	case UnzipFail
+	/// Zip fail
+	case ZipFail
+	/// Cannot overwrite Destination
+	case ZipCannotOverwrite
+	/// No Error
+	case ZipSuccess
+
+	/// User readable description
+	public var description: String {
+		switch self {
+		case .FileNotFound: return "File not found."
+		case .UnzipFail: return "Failed to unzip file."
+		case .ZipFail: return "Failed to zip file."
+		case .ZipCannotOverwrite: return "Cannot overwrite destination file."
+		case .ZipSuccess: return "Success."
+		}
+	}
+}
+
+/// Custom struct to hold required data
+public struct ProcessedFilePath {
+	/// full file path
+	let filePath: String
+
+	/// file directory path from root of zip
+	let fileDir: String
+
+	/// filename
+	let fileName: String?
+
+}
+
+
+/// Generic holdall for some needed utilities
 struct ZipUtilities {
+
+	/// Recursive function that pulls all file paths and creates an array of ProcessedFilePath
 	func processZipPaths(_ path: String, parentDir: String) -> [ProcessedFilePath]{
 		var processedFilePaths = [ProcessedFilePath]()
-		//	for path in paths {
 
 		let thisFile = File(path)
 
@@ -45,7 +91,6 @@ struct ZipUtilities {
 	}
 
 	func lastPathComponent(_ path:String) -> String {
-//		var pathArray = path.components(separatedBy: "/")
 		var pathArray = path.characters.split(separator: "/").map(String.init)
 		if pathArray.last! as String == "" {
 			pathArray.removeLast()
