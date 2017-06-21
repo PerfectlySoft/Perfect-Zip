e0c59e720217d2638e78ae5df29ac8e38143db4e
Jonathan Guthrie <jono@perfect.org>
2017-06-21 14:18:05 -0400

    refine versions

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
 	name: "PerfectZip",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/iamjono/minizip.git", versions: Version(0,0,0)..<Version(10,0,0)),
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2, minor: 0)
+		.Package(url: "https://github.com/iamjono/minizip.git", majorVersion: 0),
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2)
 	]
 )
